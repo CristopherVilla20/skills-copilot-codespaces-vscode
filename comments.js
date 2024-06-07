740327ab4db75c59d69c0a7d6db4788be6d3c6f3
Cristopher Villa <cgvilla@espol.edu.ec>
2024-06-07 01:03:57 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,43 +1,29 @@
 //create web server
-var express = require('express');
-var app = express();
-var path = require('path');
-var fs = require('fs');
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+const express = require('express');
+const app = express();
+//create server
+const http = require('http');
+const server = http.createServer(app);
+//create socket
+const io = require('socket.io')(server);
 
-//get comments
-app.get('/comments', function(req, res) {
-    fs.readFile(__dirname + '/public/comments.json', 'utf8', function(err, data) {
-        if (err) {
-            console.error(err);
-            process.exit(1);
-        }
-        res.json(JSON.parse(data));
+//create a connection
+io.on('connection', (socket) => {
+    //display the connection
+    console.log('socket connected');
+    //display the disconnection
+    socket.on('disconnect', () => {
+        console.log('socket disconnected');
+    });
+    //receive message
+    socket.on('message', (data) => {
+        console.log(data);
+        //send message
+        io.emit('message', data);
     });
 });
 
-//add comments
-app.post('/comments', function(req, res) {
-    fs.readFile(__dirname + '/public/comments.json', 'utf8', function(err, data) {
-        if (err) {
-            console.error(err);
-            process.exit(1);
-        }
-        var comments = JSON.parse(data);
-        comments.push(req.body);
-        fs.writeFile(__dirname + '/public/comments.json', JSON.stringify(comments, null, 4), function(err) {
-            if (err) {
-                console.error(err);
-                process.exit(1);
-            }
-            res.json(comments);
-        });
-    });
-});
-
-app.listen(3000, function() {
-    console.log('Server is running on localhost:3000');
+//display the port
+server.listen(3000, () => {
+    console.log('server running on port 3000');
 });
